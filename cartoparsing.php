a9fe20113c6efda5728e885d85ef38b3d48c5ab4
PreVizsla0410 <richardszigety@gmail.com>
2022-03-07 12:30:39 +0100

    Cart without Parsing

---
+++ cartoparsing.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row text-center py-5">
+            <div class="col-md-3 col-sm-6 my-3 my-md-0">
+                <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3zva0?ver=6261&q=90&m=8&h=431&w=767&b=%23FFFFFFFF&l=f&x=660&y=184&s=1460&d=821&aim=true"
+                                alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                Some text about the prodcut
+                            </p>
+                            <h5>
+                                <small><s>$ 519</s></small>
+                                <span class="price">$ 599</span>
+                            </h5>
+                            <button type="submit" class="btn btn-warning my-3" name="add">Add to cart<i
+                                    class="fas fa-shopping-cart"></i></button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+            <div class="col-md 3 col-sm-6 my-3 my-md-0">
+                <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3zva0?ver=6261&q=90&m=8&h=431&w=767&b=%23FFFFFFFF&l=f&x=660&y=184&s=1460&d=821&aim=true"
+                                alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                Some text about the prodcut
+                            </p>
+                            <h5>
+                                <small><s>$ 519</s></small>
+                                <span class="price">$ 599</span>
+                            </h5>
+                            <button type="submit" class="btn btn-warning my-3" name="add">Add to cart<i
+                                    class="fas fa-shopping-cart"></i></button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+            <div class="col-md 3 col-sm-6 my-3 my-md-0">
+                <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3zva0?ver=6261&q=90&m=8&h=431&w=767&b=%23FFFFFFFF&l=f&x=660&y=184&s=1460&d=821&aim=true"
+                                alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                Some text about the prodcut
+                            </p>
+                            <h5>
+                                <small><s>$ 519</s></small>
+                                <span class="price">$ 599</span>
+                            </h5>
+                            <button type="submit" class="btn btn-warning my-3" name="add">Add to cart<i
+                                    class="fas fa-shopping-cart"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md 3 col-sm-6 my-3 my-md-0">
+                <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3zva0?ver=6261&q=90&m=8&h=431&w=767&b=%23FFFFFFFF&l=f&x=660&y=184&s=1460&d=821&aim=true"
+                                alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                Some text about the prodcut
+                            </p>
+                            <h5>
+                                <small><s>$ 519</s></small>
+                                <span class="price">$ 599</span>
+                            </h5>
+                            <button type="submit" class="btn btn-warning my-3" name="add">Add to cart<i
+                                    class="fas fa-shopping-cart"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
